0c95d5df484541d26e2d60c3e6a570e32d62c108
Luca <l.valnegri@datamaps.co.uk>
2019-04-11 18:40:28 +0100

    Update r_packages.R

--- setup_cloud_machine_data_science/r_packages.R
+++ setup_cloud_machine_data_science/r_packages.R
@@ -1,4 +1,3 @@
-# remember to first cd into the subfolder (~/scripts/r_packages/)
 pkgs <- readLines(file('r_packages.lst'))
 pkgs <- pkgs[!sapply(pkgs, require, char = TRUE)]
 if(length(pkgs) > 0) install.packages(pkgs)
