b5a2fafe147d9c54e815d13a7877b90bffde4ff8
Luca Valnegri <l.valnegri@datamaps.co.uk>
2019-04-14 23:34:00 +0100

    first commit

---
+++ gender_paygap/04-data_viz_static.R
@@ -0,0 +1,10 @@
+#######################################
+# UK GENDER PAY GAP - DATA VIZ STATIC #
+#######################################
+
+# load packages
+pkgs <- c('data.table', 'fst', 'ggplot2')
+invisible(lapply(pkgs, require, character.only = TRUE))
+
+# load data
+
